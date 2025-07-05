28868153e6ad764e62634c269f444f6006bc5431
Islomjonsdev <islomjonmukhammadaliev4@gmail.com>
2025-07-05 23:36:34 +0500

    header fixed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -118,7 +118,7 @@ const Navbar = () => {
             <div className="flex flex-col gap-[25px]">
               <ul className="flex flex-col gap-[25px]">
                 {linkData.map((link, id) => (
-                  <li key={id}>
+                  <li key={id} onClick={() => setIsOpen(false)}>
                     <a
                       href={link.to}
                       className="relative text-white font-semibold after:content-[''] after:absolute after:left-0 after:bottom-[-2px] after:w-0 after:h-[2px] after:bg-white after:transition-all after:duration-300 hover:after:w-full"
